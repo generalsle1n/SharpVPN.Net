411f6f753a677971b819f45894dda3189e6de692
generalsle1n <niels.schuler@web.de>
2024-02-10 09:22:54 +0100

    Added Route Record

--- SharpVPN.Net.GW/Network/Protocols/Model/IPv4RouteRecord.cs
+++ SharpVPN.Net.GW/Network/Protocols/Model/IPv4RouteRecord.cs
@@ -1,6 +1,10 @@
-﻿namespace SharpVPN.Net.GW;
+﻿using System.Net;
+
+namespace SharpVPN.Net.GW;
 
 public class IPv4RouteRecord
 {
-
+    internal IPNetwork? Destination { get; init; }
+    internal IPAddress? Gateway { get; init; }
+    internal int Priorty { get; init; }
 }
