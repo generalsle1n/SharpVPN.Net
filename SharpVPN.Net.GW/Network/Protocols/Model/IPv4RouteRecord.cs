eee1a0ad6225d45ac4e3878a8d5d2352ba549d95
generalsle1n <niels.schuler@web.de>
2024-02-25 12:11:32 +0100

    Added Interface to Route

--- SharpVPN.Net.GW/Network/Protocols/Model/IPv4RouteRecord.cs
+++ SharpVPN.Net.GW/Network/Protocols/Model/IPv4RouteRecord.cs
@@ -1,10 +1,12 @@
 ﻿using System.Net;
+using SharpPcap.LibPcap;
 
 namespace SharpVPN.Net.GW;
 
 public class IPv4RouteRecord
 {
-    internal IPNetwork? Destination { get; init; }
+    internal IPNetwork2? Destination { get; init; }
     internal IPAddress? Gateway { get; init; }
     internal int Priorty { get; init; }
+    internal LibPcapLiveDevice Interface { get; set; }
 }
