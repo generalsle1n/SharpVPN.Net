5a2b8942bf38672ed95490347dd17c8f8ce46646
generalsle1n <niels.schuler@web.de>
2024-03-02 16:02:47 +0100

    Added Host for App Connection

--- SharpVPN.Net.Client/Program.cs
+++ SharpVPN.Net.Client/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using SharpVPN.Net.Client;
+using SharpVPN.Net.Client.Network;
 
-Console.WriteLine("Hello, World!");
+const string SettingsFileName = "settings.json";
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .CreateLogger();
+
+var c = new ConfigurationBuilder()
+    .AddJsonFile(SettingsFileName, false, true)
+    .Build();
+
+var Builder = new HostBuilder()
+    .UseSerilog()
+    .ConfigureAppConfiguration((config) =>
+    {
+        config.AddJsonFile(SettingsFileName, false, true);
+    })
+    .ConfigureServices((service) =>
+    {
+        service.AddSingleton<ServerConnectionHandler>();
+        service.AddHostedService<Gateway>();
+    });
+
+IHost App = Builder.Build();
+
+await App.RunAsync();
