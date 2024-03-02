dfefda92e1ed3f607a348b089560ed12cb573ee2
generalsle1n <niels.schuler@web.de>
2024-03-02 16:14:01 +0100

    Added Server Hanlder

--- SharpVPN.Net.GW/Network/Protocols/ClientHandler.cs
+++ SharpVPN.Net.GW/Network/Protocols/ClientHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Net;
+using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SharpVPN.Net.GW.Network.Entities;
@@ -12,6 +14,9 @@ public class ClientHandler
     private readonly ILogger<ClientHandler> _logger;
     private readonly IConfiguration _config;
     private TcpListener _listener;
+    private X509Certificate _cert;
+    private const string _certFileName = "server.crt";
+    private const string _certKeyFileName = "server.key";
 
     public ClientHandler(ILogger<ClientHandler> Logger, IConfiguration Config)
     {
@@ -19,12 +24,26 @@ public class ClientHandler
         _config = Config;
 
         _port = _config.GetValue<int>("Gateway:Server:Port");
+        _cert = LoadCert();
     }
     public void SetupServer(INetworkInterface Interface)
     {
         _listener = new TcpListener(new IPEndPoint(Interface.IPAddress, _port));
         _listener.Start();
-
+        _listener.BeginAcceptTcpClient(TCPClientHandler, _listener);
         _logger.LogInformation($"Started Server {_listener.LocalEndpoint}");
+    }
+    private void TCPClientHandler(IAsyncResult Result)
+    {
+        TcpClient Client = _listener.EndAcceptTcpClient(Result);
+        using (SslStream Stream = new SslStream(Client.GetStream()))
+        {
+            Stream.AuthenticateAsServer(_cert, false, false);
+            Stream.Read(Data);
+        }
+    }
+    private X509Certificate LoadCert()
+    {
+        return new X509Certificate2("server.pfx", "password");
     }
 }
